d821916668afe6717fdde7e41e07c87ff6a1f346
Zach Weaver <49159969+Kiyoshika@users.noreply.github.com>
2020-10-28 22:46:28 -0400

    Adding random seed
    Although not really necessary, decided to add random seed to the uniform sampler.

--- MonteIntegral.cpp
+++ MonteIntegral.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <ctime>
 
 // function to integrate
 double function(double x, double y) {
@@ -7,8 +8,7 @@ double function(double x, double y) {
 }
 
 double double_integrate(double low_0, double high_0, double low_1, double high_1, int iter = 100000) {
-    std::random_device rd;
-    std::default_random_engine reng(rd());
+    std::default_random_engine reng(time(0));
     std::uniform_real_distribution<> unif_0(low_0, high_0);
     std::uniform_real_distribution<> unif_1(low_1, high_1);
 
